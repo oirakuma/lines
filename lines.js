a99a82f4b811285a0da71610d0eba2d3936aa604
inui <oirakuma@gmail.com>
2015-06-18 11:03:47 +0900

    Added checking moveable

--- lines.js
+++ lines.js
@@ -27,18 +27,13 @@
   function createBall() {
     var x = ballImages[Math.floor(Math.random()*ballImages.length)];
     var img = $('<img class="ball" src="images/'+x+'">');
-    img.click(function(e){
-      e.stopPropagation();
-      $(this).toggleClass("selected");
-      selectedBall = this;
-    });
     return img;
   }
 
   function copyNext() {
     for (var i = 0; i < 3; i++) {
       var img = $("#next img")[0];
-      $(shuffle($("td:empty"))[0]).html(img);
+      $(shuffle($("#content td:empty"))[0]).append($(img).effect("pulsate"));
     }
   }
 
@@ -58,7 +53,7 @@
       var img = $(tds[x*SIZE+y]).find("img");
       if (img.length == 0) return false;
       var src1 = img.attr("src")
-      for (var k = 1; k < 3; k++) {
+      for (var k = 1; k < 5; k++) {
         var x2 = x+k*vx;
         var y2 = y+k*vy;
         if (x2 < 0 || x2 >= SIZE || y2 < 0 || y2 >= SIZE) return false;
@@ -72,7 +67,7 @@
     }
 
     function eraseLine(x, y, vx, vy) {
-      for (var k = 0; k < 3; k++) {
+      for (var k = 0; k < 5; k++) {
         var img = $(tds[(x+k*vx)*SIZE+(y+k*vy)]).find("img");
         $(img).effect("explode", null, "slow", function(){
           $(this).remove();
@@ -82,39 +77,92 @@
       $("#score").text(score+25);
     }
 
+    var erased = false;
     for (var i = 0; i < SIZE; i++) {
       for (var j = 0; j < SIZE; j++) {
-        if (isLined(i, j,  1,  0))
+        if (isLined(i, j,  1,  0)) {
+          erased = true;
           eraseLine(i, j,  1,  0);
-        if (isLined(i, j,  0,  1))
+        }
+        if (isLined(i, j,  0,  1)) {
+          erased = true;
           eraseLine(i, j,  0,  1);
-        if (isLined(i, j,  1,  1))
+        }
+        if (isLined(i, j,  1,  1)) {
+          erased = true;
           eraseLine(i, j,  1,  1);
-        if (isLined(i, j,  1, -1))
+        }
+        if (isLined(i, j,  1, -1)) {
+          erased = true;
           eraseLine(i, j,  1, -1);
+        }
       }
     }
+    return erased;
+  }
+
+  var cache = null;
+
+  function canMove(x1, y1, x2, y2, level) {
+    if (x1 < 0 || x1 >= SIZE || y1 < 0 || y1 >= SIZE) return false;
+
+    //check cache
+    if (cache[x1*SIZE+y1]) return false;
+    cache[x1*SIZE+y1] = true;
+
+    //check ball
+    if (level > 0 && $($("#content td")[x1*SIZE+y1]).find("img").length > 0)
+      return false;
+
+    //check goal
+    if (x1 == x2 && y1 == y2) return true;
+
+    if (canMove(x1  , y1+1, x2, y2, level+1)) return true;
+    if (canMove(x1+1, y1  , x2, y2, level+1)) return true;
+    if (canMove(x1  , y1-1, x2, y2, level+1)) return true;
+    if (canMove(x1-1, y1  , x2, y2, level+1)) return true;
+
+    return false;
+  }
+
+  function createTd(x, y) {
+    var td = $('<td></td>').addClass("x"+x+"-"+y).click(function(){
+      var img = $(this).find("img");
+      if (img.length == 0) {
+        if (selectedBall) {
+          var x1 = parseInt($(selectedBall).attr("class")[1]);
+          var y1 = parseInt($(selectedBall).attr("class")[3]);
+          var x2 = parseInt($(this).attr("class")[1]);
+          var y2 = parseInt($(this).attr("class")[3]);
+
+          cache = {};
+          if (!canMove(x1, y1, x2, y2, 0)) return;
+          $(this).append($(selectedBall).find("img"));
+          $(selectedBall).find("img").removeClass("selected");
+          selectedBall = null;
+          setTimeout(function(){
+            var erased = checkLine();
+            if (!erased) {
+              copyNext();
+              createNext();
+            }
+          }, 100);
+        }
+      } else {
+        $(selectedBall).find("img").removeClass("selected");
+        img.toggleClass("selected");
+        selectedBall = this;
+      }
+    });
+    return td;
   }
 
   function render() {
     var table = $("<table></table>").attr("cellspacing",1);
     for (var i = 0; i < SIZE; i++) {
       var tr = $("<tr></tr>");
-      for (var j = 0; j < SIZE; j++) {
-        var td = $('<td></td>').click(function(){
-          if (selectedBall) {
-            $(this).append(selectedBall);
-            $(selectedBall).removeClass("selected");
-            selectedBall = null;
-            setTimeout(function(){
-              checkLine();
-              copyNext();
-              createNext();
-            }, 100);
-          }
-        });
-        tr.append(td);
-      }
+      for (var j = 0; j < SIZE; j++)
+        tr.append(createTd(i, j));
       table.append(tr);
     }
     $("#content").html(table);
